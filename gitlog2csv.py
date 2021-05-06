2c25be0005c9aebe48f8fa5929f957ae8425e1e4
Rolf Offermanns <rof@sysgo.com>
2021-05-06 16:04:10 +0200

    Adapt to changed website structure

--- gitlog2csv.py
+++ gitlog2csv.py
@@ -15,24 +15,42 @@ State = Enum('State', 'INZIDENZ DATE')
 state = State.DATE
 current_date = ""
 date_inz = {}
+old_parser = True
+capture = False
 locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
 for line in output.split("\n"):
-    if re.match(r'^\+.*Fallzahlen', line):
-        date_match = re.match(r'^.*Stand: [^,]*, ([^,]*).*', line)
-        date = date_match[1]
-        current_date = datetime.strptime(date, "%d. %B %Y")
-        state = State.DATE
+    if re.match(r'^commit 5f3c2d45a45fb526a07e1f9b4dc880b638da6dd3', line):
+        old_parser = False
         continue
 
-    if re.match(r'^\+.*Inzidenz von', line):
-        if state != State.DATE:
+    if old_parser:
+        if re.match(r'^\+.*Fallzahlen', line):
+            date_match = re.match(r'^.*Stand: [^,]*, ([^,]*).*', line)
+            date = date_match[1]
+            current_date = datetime.strptime(date, "%d. %B %Y")
+            state = State.DATE
             continue
-        inz_match = re.match(r'^.*7-Tage-Inzidenz von ([0-9,]*).*', line)
-        inzidenz = inz_match[1]
-        date_inz[current_date.strftime("%Y-%m-%d")] = inzidenz.replace(",", ".")
-        state = State.INZIDENZ
-        continue
 
+        if re.match(r'^\+.*Inzidenz von', line):
+            if state != State.DATE:
+                continue
+            inz_match = re.match(r'^.*7-Tage-Inzidenz von ([0-9,]*).*', line)
+            inzidenz = inz_match[1]
+            date_inz[current_date.strftime("%Y-%m-%d")] = inzidenz.replace(",", ".")
+            state = State.INZIDENZ
+            continue
+    else:
+        if re.match(r'^ <p><strong>7-Tage-Inzidenz', line):
+            capture = True
+            continue
+        if capture:
+            new_inzidenz = re.match(r'^\+<li>(.*) - (.*)</li>', line)
+            if new_inzidenz:
+                current_date = datetime.strptime(new_inzidenz[1], "%d. %B %Y")
+                inzidenz = new_inzidenz[2].replace(",", ".")
+                date_inz[current_date.strftime("%Y-%m-%d")] = inzidenz
+                capture = False
+                continue
 
 csv_writer = csv.writer(sys.stdout, delimiter=',')
 for date in date_inz.keys():
